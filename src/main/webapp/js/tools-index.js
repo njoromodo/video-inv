f0e6e2c292fbf018a54ead545a737357196298e7
Tom Paulus <tom@tompaulus.com>
2016-03-21 14:19:14 -0700

    Tool Improvements
    Tools page now looks better and Item Tools doesn't hiccup the first time an item ID is entered.

---
+++ src/main/webapp/js/tools-index.js
@@ -0,0 +1,18 @@
+/**
+ * Tools Page Button Routes
+ *
+ * Created by tpaulus on 3/21/16.
+ */
+
+var user_id = JSON.parse(getCookie("current_user")).pubID;
+if (user_id == null) {
+    window.top.location = "../403.html";
+}
+
+function items() {
+    window.top.location = "items.html";
+}
+
+function users() {
+    window.top.location = "users.html";
+}
