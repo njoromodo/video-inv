01759fbad0b552291bd22b5643c3b80e65b7bc5c
Tom Paulus <tom@tompaulus.com>
2016-05-14 21:52:55 -0700

    Moved Print Label Function to Common
    Since several functions/pages need to print Labels, and it is the same method for all. It has been moved to `common.js`. The Framework still needs to be included on every page where printing will occur.

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -70,3 +70,37 @@ function getMidnight() {
 function getPosition(str, m, i) {
     return str.split(m, i).join(m).length;
 }
+
+/**
+ * Send the Label XML to the Printer via the Dymo JS Framework
+ * @param xml Label XML
+ */
+function makeLabel(xml) {
+    try {
+        var label = dymo.label.framework.openLabelXml(xml);
+
+        // select printer to print on
+        // for simplicity sake just use the first LabelWriter printer
+        var printers = dymo.label.framework.getPrinters();
+        if (printers.length == 0)
+            throw "No DYMO printers are installed. Install DYMO printers.";
+
+        var printerName = "";
+        for (var i = 0; i < printers.length; ++i) {
+            var printer = printers[i];
+            if (printer.printerType == "LabelWriterPrinter") {
+                printerName = printer.name;
+                break;
+            }
+        }
+
+        if (printerName == "")
+            throw "No LabelWriter printers found. Install LabelWriter printer";
+
+        // finally print the label
+        label.print(printerName);
+    }
+    catch (e) {
+        alert(e.message || e);
+    }
+}
